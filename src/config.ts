08022f4c90263dd1a856699b19b1977ac318cd2d
Aimer0 <35005577+Aimer0@users.noreply.github.com>
2025-02-11 13:29:41 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export let PROXY_CODE_SECRET = 'Easy-Reverse-Proxy'; // 代理码密钥
 // 代理
 export let PROXIES: Proxy[] = [
     {
-        domain: "www.example.com",
+        domain: "proxydzg.zeabur.app",
         url: "https://www.google.com/",
         enable: true
     }
